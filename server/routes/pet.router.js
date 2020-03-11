03af8597cf42b5a9b41e768fa46b5009b36f4333
demkantor <demkantor@gmail.com>
2020-03-11 15:02:53 -0500

    mixed auth if else for api/pets

--- server/routes/pet.router.js
+++ server/routes/pet.router.js
@@ -7,13 +7,17 @@ router.get('/', (req, res) => {
     console.log('/pet GET route');
     console.log('is authenticated?', req.isAuthenticated());
     console.log('user', req.user);
-    let queryText = `SELECT * FROM "pet"`;
-    pool.query(queryText).then((result) => {
-        res.send(result.rows);
-    }).catch((error) => {
-        console.log(error);
-        res.sendStatus(500);
-    });
+    if(req.isAuthenticated()){
+        let queryText = `SELECT * FROM "pet"`;
+        pool.query(queryText).then((result) => {
+            res.send(result.rows);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+    }else {
+        res.sendStatus(403);
+    }
 });
 
 // This route *should* add a pet for the logged in user
